6cf1923c3fc4833e5a73ad469d6c5702188ce18e
Kevin Najera <kevinalex360boyka@gmail.com>
2021-12-20 19:04:09 -0600

    Version final

--- src/main/java/com/fcfm/alumnos/Main.java
+++ src/main/java/com/fcfm/alumnos/Main.java
@@ -13,15 +13,25 @@ import java.util.Scanner;
 import com.fcfm.alumnos.dao.AlumnosDAO;
 import com.fcfm.alumnos.dto.AlumnosDTO;
 
-public class Main {
+import Facultad.FacultadNombre;
 
+public class Main {
 
 	public static void main(String[] args) {
 
 		int opcion = -1;
+		String Facultad;
 
 		Scanner entrada = new Scanner(System.in);
 		AlumnosDAO alumnosDAO = new AlumnosDAO();
+		FacultadNombre f = FacultadNombre.getInstance();
+
+		// aqui pido facultad//
+		System.out.println("\nIngresa el nombre de la facultad: ");
+		Facultad = entrada.nextLine();
+		f.setNombreFacu(Facultad);
+		
+
 		try {
 
 			while (opcion != 0) {
@@ -31,17 +41,17 @@ public class Main {
 				opcion = entrada.nextInt();
 				switch (opcion) {
 				case 1:// insertar datos//
-					
-                    alumnosDAO.Insert();
+
+					alumnosDAO.Insert();
 					System.out.println("Alumno insertado correctamente");
 					break;
 				case 2:// modificar alumno//
-	
-                    alumnosDAO.UpdateAlumno();
+
+					alumnosDAO.UpdateAlumno();
 					System.out.println("Alumno actualizado correctamente");
 					break;
-				case 3://Borrar de tabla//
-					
+				case 3:// Borrar de tabla//
+
 					alumnosDAO.DeleteAlumno();
 					System.out.println("Alumno Borrado correctamente\n\n");
 
@@ -65,23 +75,21 @@ public class Main {
 
 		}
 
-	 }
-	
+	}
 
-	public static void show (List<AlumnosDTO> rs) {
-	    	
-	      for(AlumnosDTO actual : rs) {
-	    	  System.out.println(
-	    			  actual.getId() +" "
-	    			  + actual.getMatricula() +" "
-	    			  + actual.getNombre() + " "
-	    			  + actual.getApellido() + " "
-	    			  + actual.getGrupo() + " "
-	    			  + actual.getMateria() + " "
-	    			  + actual.getCalificacion()
-	    			  
-	    			  );
-	      }
+	public static void show(List<AlumnosDTO> rs) {
+
+		FacultadNombre f = FacultadNombre.getInstance();
+		for (AlumnosDTO actual : rs) {
+			System.out.println(
+					actual.getId() + " " + actual.getMatricula() + " " + actual.getNombre() + " " + actual.getApellido()
+							+ " " + actual.getGrupo() + " " + actual.getMateria() + " " + actual.getCalificacion()
+
+			);
+			System.out.println("Pertenece a: " + f.getNombreFacu()+"\n");
+		}
+
+		
 
 	}
 
